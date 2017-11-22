6e6c5d451c1f99cfedd10c8dcb46d5a842150a2a
Quentin Bellanger <hello@quentin-bellanger.com>
2017-11-22 16:52:55 +0100

    Add create action for websites

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -5,4 +5,22 @@ class WebsitesController < ApplicationController
     @templates = Template.all
     @website = Website.new
   end
+
+  def create
+    @website = Website.new(website_params)
+    @user = current_user
+    @website.user = @user
+
+    if @website.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def website_params
+    params.require(:website).permit(:template_id)
+  end
 end
