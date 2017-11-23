c3c8cb17fd99489e4297786fbcff31d01523f040
Numa Jequier <numa.jequier@gmail.com>
2017-11-23 12:34:58 +0100

    routes updated and button redirect to website Edit

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,7 +2,8 @@ class WebsitesController < ApplicationController
   layout 'builder'
 
   def edit
-    @website = Website.find(website_params)
+    @templates = Template.all
+    @website = Website.find(params[:id])
   end
 
   def update
@@ -10,35 +11,33 @@ class WebsitesController < ApplicationController
   end
 
   def new
-    # @templates = Template.all
-    # @website = Website.new
-    website = Website.new(template_id: 1, user_id: current_user.id)
-    website.save
-    redirect_to edit_website_path(website)
+    @templates = Template.all
+    @website = Website.new
   end
 
   def create
-    @website = Website.new(website_params)
-    @user = current_user
-    @website.user = @user
+    @website = current_user.websites.create(template: Template.first)
+    redirect_to edit_website_path(@website)
+    # @website = Website.new(website_params)
+    # @user = current_user
+    # @website.user = @user
 
-    if @website.save
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.html { render 'new' }
-        format.js
-      end
-    end
+    # if @website.save
+    #   respond_to do |format|
+    #     format.html { redirect_to root_path }
+    #     format.js
+    #   end
+    # else
+    #   respond_to do |format|
+    #     format.html { render 'new' }
+    #     format.js
+    #   end
+    # end
   end
 
   private
 
   def website_params
-    ActionController::Parameters.permit_all_parameters = true
-    # params.require(:website).permit(:template_id, :id)
+    params.require(:website).permit(:template_id, :id)
   end
 end
