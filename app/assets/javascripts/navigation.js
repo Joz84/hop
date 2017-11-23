7b60a1fa253715885311574023175c5f86c37f24
Quentin Bellanger <hello@quentin-bellanger.com>
2017-11-23 15:08:03 +0100

    Toggle active item in toolbar on submit

---
+++ app/assets/javascripts/navigation.js
@@ -0,0 +1,18 @@
+const submitButton = document.querySelector('#website-submit');
+const toolbarItems = document.querySelectorAll('.toolbar li');
+const activeClass = 'toolbar-menu-item-active';
+
+submitButton.addEventListener('click', (e) => {
+
+  if (toolbarItems[0].classList.contains(activeClass)) {
+
+    toolbarItems[0].classList.remove(activeClass);
+    toolbarItems[1].classList.add(activeClass);
+
+  } else if (toolbarItems[1].classList.contains(activeClass)) {
+
+    toolbarItems[1].classList.remove(activeClass);
+    toolbarItems[2].classList.add(activeClass);
+
+  }
+});
