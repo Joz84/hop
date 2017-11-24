36fb88d02f15947c827ce904735ecabeae74921d
Numa Jequier <numa.jequier@gmail.com>
2017-11-24 12:54:42 +0100

    inject HTML to builder website, ajax done

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -13,24 +13,32 @@ class WebsitesController < ApplicationController
   def update
     @website = Website.find(params[:id])
     @website.update(website_params)
-    redirect_to edit_website_path(@website)
+    raise
+    if @website.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'update' }
+        format.js
+      end
+    end
+    # redirect_to edit_website_path(@website)
   end
 
   def create
     @website = current_user.websites.create(template: random_template)
     redirect_to edit_website_path(@website)
+  end
 
-    # if @website.save
-    #   respond_to do |format|
-    #     format.html { redirect_to root_path }
-    #     format.js
-    #   end
-    # else
-    #   respond_to do |format|
-    #     format.html { render 'new' }
-    #     format.js
-    #   end
-    # end
+  # GET /websites/:id/template => { template: ":template_slug"}
+  def template
+    @website = Website.find(params[:id])
+    template_slug = params[:template_slug]
+    template_html = render_to_string partial: "templates/#{template_slug}", website: @website
+    render json: { html: template_html }.to_json
   end
 
   private
