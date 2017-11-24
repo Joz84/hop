150ffe2a077051761a409a968ad9df9a91fc40fa
Numa Jequier <numa.jequier@gmail.com>
2017-11-24 17:52:00 +0100

    simplificate tooltip

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,5 +1,12 @@
-var tooltip = document.querySelector(".tooltip-welcome");
-var crossClose = tooltip.querySelector(".cross-icon");
-crossClose.addEventListener("click", function(){
-  tooltip.classList.add("toolhide");
+// var tooltipWelcome = document.querySelector(".tooltip-welcome");
+// var crossCloseWelcome = tooltipWelcome.querySelector(".cross-icon");
+// crossCloseWelcome.addEventListener("click", function(){
+//   tooltipWelcome.style.display = 'none';
+// });
+
+var tooltipNavigate = document.querySelector(".tooltip-navigate");
+console.log(tooltipNavigate)
+var crossCloseNavigate = tooltipNavigate.querySelector(".cross-icon");
+crossCloseNavigate.addEventListener("click", function(){
+  tooltipNavigate.style.display = 'none';
 });
