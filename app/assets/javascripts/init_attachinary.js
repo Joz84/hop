f1e86b74e5000dc4610a53743761709b69de8d71
Quentin Bellanger <hello@quentin-bellanger.com>
2017-11-28 16:21:38 +0100

    Live edit image upload

--- app/assets/javascripts/init_attachinary.js
+++ app/assets/javascripts/init_attachinary.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
   $('.attachinary-input').attachinary();
 });
+
+$('.website_profile_image #website_profile_image').bind('fileuploaddone', function (e, data) {
+  // Live-update the profile image when upload succeeds
+  var profileImageUrl = data.result.secure_url;
+  var currentProfileImage = $('.template-container .template-photo');
+  currentProfileImage.attr('src', profileImageUrl);
+
+  // Hide attachinary default preview
+  $('.attachinary_container').each(function(container) {
+    container.hide();
+  });
+});
+
+$('.website_background_image #website_background_image').bind('fileuploaddone', function (e, data) {
+  // Live-update the container background image when upload succeeds
+  var backgroundImageUrl = data.result.secure_url;
+  var currentBackgroundContainer = $('.template-container');
+  currentBackgroundContainer.attr('style', "background-image: url('" + data.result.secure_url + "');");
+
+  // Hide attachinary default preview
+  $('.attachinary_container').each(function(container) {
+    container.hide();
+  });
+});
