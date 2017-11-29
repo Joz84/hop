209065eb9c913b091ffad977fa7160c8d004717b
serge cleoron <sc21417@gmail.com>
2017-11-29 22:34:50 +0100

    fixe tooltips on edit

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,11 +1,19 @@
-// var tooltipWelcome = document.querySelector(".tooltip-welcome");
-// var crossCloseWelcome = tooltipWelcome.querySelector(".cross-icon");
-// crossCloseWelcome.addEventListener("click", function(){
-//   tooltipWelcome.style.display = 'none';
+$(document).ready(function() {
+    $('.tooltip-welcome').addClass('tooltip-welcome-open')
+});
+
+var tooltipWelcome = document.querySelector(".tooltip-welcome");
+var crossCloseWelcome = tooltipWelcome.querySelector(".cross-icon");
+crossCloseWelcome.addEventListener("click", function(){
+  tooltipWelcome.style.display = 'none';
+  $('.tooltip-navigate').addClass('tooltip-navigate-open')
+});
+
+// $(document).ready(function() {
+
 // });
 
 var tooltipNavigate = document.querySelector(".tooltip-navigate");
-
 var crossCloseNavigate = tooltipNavigate.querySelector(".cross-icon");
 crossCloseNavigate.addEventListener("click", function(){
   tooltipNavigate.style.display = 'none';
