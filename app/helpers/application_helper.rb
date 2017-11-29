530b4386a1edc73b729d67b530f40d2b25fff004
Quentin Bellanger <hello@quentin-bellanger.com>
2017-11-29 12:06:36 +0100

    Add real default content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
-
   def cl_image_path_or_default(image, options = {})
     if image
       cl_image_path image.path, options
     else
-      "http://res.cloudinary.com/dy4rog9h0/image/upload/v1511798148/ymvxzef4dpputt1com2w.jpg"
+      'https://res.cloudinary.com/dy4rog9h0/image/upload/v1511951967/qnudk7b6khwivnairedi.jpg'
     end
   end
 
@@ -12,8 +11,7 @@ module ApplicationHelper
     if image
       cl_image_tag image.path, options
     else
-      image_tag "http://res.cloudinary.com/dy4rog9h0/image/upload/v1511798148/ymvxzef4dpputt1com2w.jpg", options
+      image_tag 'https://res.cloudinary.com/dy4rog9h0/image/upload/v1511953517/dorothee_yusfkt.png', options
     end
   end
-
 end
