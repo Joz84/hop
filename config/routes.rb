8c872d0f79a760a74b15950de9f3e386ebe279d7
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-09-15 23:02:01 +0200

    Reworked routes to properly match up with with what should be, subdomain-wise.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: 'websites#show', constraints: lambda { |req| req.subdomains.any? }
+  constraints lambda { |req| (req.subdomains - 'build').any? } do
+    root to: 'websites#show'
+    get 'edit', to: 'websites#edit', as: 'edit_website'
+    post '/', to: 'websites#update', as: 'website'
+  end
 
-  resources :websites, only: [:index, :show, :create, :edit, :update] do
-    member do
-      get 'template' => 'websites#template'
-    end
+  constraints subdomain: 'build' do
+    resources :websites, only: [:create]
   end
 end
