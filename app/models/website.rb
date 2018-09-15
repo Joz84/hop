e831032f1dbab6f0927bc6e5c625aeac58244724
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-09-15 23:02:45 +0200

    Take into account new reserved tenant.

--- app/models/website.rb
+++ app/models/website.rb
@@ -4,8 +4,14 @@ class Website < ApplicationRecord
   belongs_to :user
   belongs_to :template
 
+  validates :url, presence: true
+
   has_attachment :background_image
   has_attachment :profile_image
+
+  validate do
+    errors.add 'URL is a reserved keyword' if url.to_s == 'build'
+  end
 
   private
 
