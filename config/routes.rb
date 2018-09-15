bb3fda63fb6078ca6f400905774ef7319989acaa
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-09-15 23:06:45 +0200

    Fixed inattention bug in routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  constraints lambda { |req| (req.subdomains - 'build').any? } do
+  constraints lambda { |req| (req.subdomains - ['build']).any? } do
     root to: 'websites#show'
     get 'edit', to: 'websites#edit', as: 'edit_website'
     post '/', to: 'websites#update', as: 'website'
+
   end
 
   constraints subdomain: 'build' do
